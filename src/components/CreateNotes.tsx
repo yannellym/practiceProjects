a0d8095c53c204234192cc8a2c18ac2160baaa2c
yannelly mercado <yannellym@gmail.com>
2022-05-10 19:10:28 -0400

    new feature

--- src/components/CreateNotes.tsx
+++ src/components/CreateNotes.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import { Button, Form } from 'react-bootstrap';
-
+import { Alert, Button, Form } from 'react-bootstrap';
+import { Note } from "../models/notesModel"
 interface ICreateNotesProps {
+    notes: Note[],
+    setNotes: React.Dispatch<React.SetStateAction<Note[]>>
 }
 
-const CreateNotes: React.FunctionComponent<ICreateNotesProps> = (props) => {
+const CreateNotes: React.FunctionComponent<ICreateNotesProps> = ({ notes, setNotes }) => {
     const titleRef = React.useRef<HTMLInputElement | null>(null);
     const textRef = React.useRef<HTMLTextAreaElement | null>(null);
     const colorRef = React.useRef<HTMLInputElement | null>(null);
@@ -14,10 +16,22 @@ const CreateNotes: React.FunctionComponent<ICreateNotesProps> = (props) => {
         if(titleRef.current?.value === "" || textRef.current?.value === ""){
             return setError("Please fill out all fields")
         }
+        setError("");
+        setNotes([ ...notes, {
+            id: (new Date()).toString(),
+            title: (titleRef.current as HTMLInputElement).value,
+            text: (textRef.current as HTMLTextAreaElement).value,
+            color: (colorRef.current as HTMLInputElement).value,
+            date: (new Date()).toString()
+        }]);
+        
+        (titleRef.current as HTMLInputElement).value = "";
+        (textRef.current as HTMLTextAreaElement).value = "";
     }
   return (
       <>
         <h2>Create notes</h2>
+        {error && <Alert variant="danger">{error}</Alert>} 
         <Form className="mt-3 mb-3" onSubmit={(e) => handleSubmit(e) }>
             <Form.Group className="mb-3" controlId="formBasicTitle">
                 <Form.Label>Title</Form.Label>
--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -17,7 +17,7 @@ const Notes: React.FunctionComponent<INotesProps> = ( {note, handleDelete}) => {
             <Card.Body style={{backgroundColor: note.color}}>
               <Card.Title>{note.title}</Card.Title>
               <Card.Text>{note.text}</Card.Text>
-              <Card.Subtitle className="text-muted">{note.data}</Card.Subtitle>
+              <Card.Subtitle className="text-muted">{note.date}</Card.Subtitle>
               <Button className="mt-3" variant="danger" onClick={ () => handleDelete(note.id)}>Delete</Button>
             </Card.Body>
           </Card>
